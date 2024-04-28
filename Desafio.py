2f5485413759dec24e95c3fd8e943e8e68c3b19f
Fernando Braz <fernandobraz88@gmail.com>
2024-04-28 00:12:31 -0300

    Iniciando a V3

--- Desafio.py
+++ Desafio.py
@@ -1,69 +1,21 @@
-menu = '''
-### BANCO DIO ###
+from abc import ABC, abstractproperty, abstractmethod
+from datetime import datetime
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+class Cliente:
+    pass
 
-'''
 
-saldo = 0
-limite = 500
-extrato = []
-numero_saques = 0
-LIMITE_SAQUES = 3
+class PessoaFisica(Cliente):
+    pass
 
-while True:
+class Conta:
+    pass
 
-    opcao = input(menu).lower()
+class ContaCorrente(Conta):
+    pass
 
-    if opcao =="d":
-        print("===== Deposito =====\n")
-        deposito = float(input("Digite o valor de depósito: "))
-        
-        if deposito > 0:
-            saldo += deposito
-            extrato.append(f"Deposito: R${deposito:.2f}")
-            print(f"Deposito realizado com sucesso, saldo atual: R${saldo:.2f}")
-        
-        else:
-            print("Valor de depósito inválido")
-    
-    elif opcao == "s":
-        print("===== Saque =====\n")
-        saque = float(input("Digite o valor de saque: "))
-        
-        if numero_saques < LIMITE_SAQUES:
-            
-            if saque <= saldo and saque <= 500:
-                saldo -= saque
-                numero_saques += 1
-                extrato.append(f"Saque: R${saque:.2f}")
-                print(f"Saque realizado com sucesso, saldo atual: R${saldo:.2f}")
-            
-            elif saque > saldo:
-                print("Saldo Insuficiente")
-            
-            elif saque > 500:
-                print("Limite permitido por saque: R$500,00")
-        
-        else:
-            print("Número de saques diários excedido.")
-    
-    elif opcao == "e":
-        print("===== Extrato =====\n")
-        
-        if len(extrato) != 0:
-            
-            for i in extrato:
-                print(i)
-        
-        else:
-            print("Nenhuma operação realizada ainda")
+class Historico:
+    pass
 
-    elif opcao == "q":
-        break
-
-    else:
-        print("Operação invalida, por favor selecione novamente a operação desejada.")
+class Transacao(ABC):
+    pass
